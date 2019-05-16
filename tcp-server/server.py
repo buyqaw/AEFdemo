4b9aa8a5c0e52e2d6e2103e45b8bd4bb9e467929
BiggyBaron <dr.cleverest@gmail.com>
2019-05-17 00:57:35 +0600

    Telegram test

--- tcp-server/server.py
+++ tcp-server/server.py
@@ -58,6 +58,7 @@ def send_tlg_msg(msg):
         try:
             from requests import Request, Session
             # print(bot.sendMessage(str(id), str(msg)))
+            msg = "Ровно в " + str(datetime.now()) + " через дверь прошел пользователь под именем *" + msg + "*. Спасибо!"
             print(requests.get(head+msg))
         except:
             print("No connection to telegram")
